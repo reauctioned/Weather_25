ce0968d464df374e7f086c3a43bb83d78e21f6c4
Saurav <sauravgamer441@gmail.com>
2024-08-13 23:49:06 +0545

    possibly fixed key.env

--- my-project/src/components/weather/index.jsx
+++ my-project/src/components/weather/index.jsx
@@ -1,26 +1,30 @@
 import { useState } from "react";
 import Search from "../search";
 
-export default function Weather(){
-    const [search,setSearch] = useState('')
-    const [loading,setLoading] = useState(false)
-    const [weather,setWeather] = useState(null)
+export default function Weather() {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weather, setWeather] = useState(null);
 
-    async function fetchWeatherdata(){
-            try{
-                const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid={API key}`)
-            }catch(e){
+  async function fetchWeatherdata(param) {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+        
+      );
+      const data = await res.json()
+      console.log(data, 'data');
+      
+    } catch (e) {}
+  }
 
-            }
-    }
+   function handleSearch() {
+        fetchWeatherdata(search)
+  }
 
-    async  function handleSearch(){
-
-    }
-
-    return(
-        <div>
-          <Search search={search} setSearch={setSearch}/>
-        </div>
-    )
+  return (
+    <div>
+      <Search search={search} setSearch={setSearch} handleSearch={handleSearch} />
+    </div>
+  );
 }
