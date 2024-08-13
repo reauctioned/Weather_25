0731b9fcd0a930001a842ec5c4cff8a09990b0ac
Saurav <sauravgamer441@gmail.com>
2024-08-13 23:23:44 +0545

    add styles to input and button

--- my-project/src/components/search/index.jsx
+++ my-project/src/components/search/index.jsx
@@ -1,17 +1,19 @@
+export default function Search({ search, setSearch }) {
+  function handleSearch() {}
 
-
-export default function Search({search,setSearch}){
-    return(
-        <div className="search">
-            <input type="text"
-            className="city-search" 
-            placeholder="ohio"
-            name="search"
-            value={search}
-            onChange={(event)=> setSearch(event.target.value)}/>
-            <button className="search-button" onClick={handleSearch} >
-                Search
-            </button>
-        </div>
-    )
+  return (
+    <div className="w-full  flex    justify-around  items-center   mt-[10px]  mb-[30px]">
+      <input
+        type="text"
+        className="w-9/12  h-45px  border-solid   border  rounded-lg  text-base text-[#555555]  outline-none  py-[2px] px-[15px]"
+        placeholder="ohio"
+        name="search"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <button className="border-none rounded-[8px] bg-[#555555] text-[white]  outline-none py-[6px] px-[6px] cursor-pointer" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
 }
